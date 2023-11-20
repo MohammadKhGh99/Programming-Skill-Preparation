305ca59fcccad8d56516c76276f94c4f3824a954
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2023-11-20 22:53:27 +0200

    #12 Exercise Done.

--- ex12.py
+++ ex12.py
@@ -1,7 +1,18 @@
 # 1 - Suppose you're building a logging library for a web application that needs to keep track of all
 #     requests and responses. You want to use the Singleton pattern to ensure that there's only one instance of
 #     the logger class throughout the application.
+class Logger:
+    instance = None
+    
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance = super(Logger, cls).__new__(cls)
+        return cls.instance
 
 
 if __name__ == '__main__':
-    pass
+    # let's check what we did
+    logger1 = Logger()
+    logger2 = Logger()
+    
+    print("The Singleton Worked? " + str(logger1 is logger2))
